7b01951f287feaab70c195359d63e9e8ddee80ea
ramkumar-2497 <ramkumar97.ravi@gmail.com>
2024-06-29 11:20:14 +0530

    Add files via upload

---
+++ seleniumSample/SeleniumSampleCode.java
@@ -0,0 +1,13 @@
+package seleniumSample;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumSampleCode {
+
+	public static void main(String[] args) {
+			ChromeDriver driver=new ChromeDriver();
+			driver.get("http://leaftaps.com/opentaps/control/main");
+			}
+
+
+	}
